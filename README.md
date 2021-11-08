18dcfd5cf473ba69b87db6419492132a5060d8eb
Lichapa <lichapau@gmail.com>
2021-11-08 18:17:22 +0200

    Add files for a comit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Peer-Feedback
+# ToDoList
+
+> The aim of the project is to create a to do list and use webpack to bundle Javascript.
+
+![screenshot](./screenshot.PNG)
+
+## Project objectives.
+- Use webpack to bundle JavaScript.
+- Practice how to use proper ES6 syntax.
+- Use ES6 modules to write modular JavaScript.
+- Write a function to render dynamically created list of tasks in the dedicated placeholder. 
+- Load CSS using webpack Style/CSS Loader
+
+## Built With
+
+- HTML/CSS
+- JavaScript
+- webpack
+
+# Live Demo
+> The live Demo of the project is:
+- [On Netlify](https://618639f341845acb2eca1f8c--jovial-haibt-337d30.netlify.app/)
+
+- [On GitHub Pages](https://lichapa.github.io/ToDoList/dist/)
+
+
+## Getting Started
+
+> Visit my [repository](https://github.com/Lichapa/ToDoList.git)
+> Clone the repository in your code editor using the URL above.
+> Change directory into the repository and make your desried changes.
+## Author
+
+@Lichapa: Mphatso Lichapa
+
+- GitHub: [@Lichapa](https://github.com/Lichapa)
+- Twitter: [@LichapaMphatso](https://twitter.com/LichapaMphatso)
+- LinkedIn: [mphatsolichapa](https://www.linkedin.com/in/mphatsolichapa)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/Lichapa/ToDoList/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- 
+## 📝 License
+
+This project is [MIT](https://mit-license.org/) licensed.
---
+++ src/index.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-restricted-syntax */
+import './style.css';
+import { getTaskLS } from './interactive.js';
+import addtodoItem, { manipulate, clearCompleted } from './addRemove.js';
+
+const toDoList = document.querySelector('.toDoList');
+const toDoBtn = document.querySelector('.btnToDo');
+const clearBtn = document.querySelector('.btn-clear');
+
+toDoList.addEventListener('click', manipulate);
+toDoBtn.addEventListener('click', addtodoItem);
+clearBtn.addEventListener('click', clearCompleted);
+
+function loadTodo() {
+  const toDoObjects = getTaskLS();
+  let toDoListItems = '';
+  for (const toDo of toDoObjects) {
+    if (toDo.completed === true) {
+      toDoListItems += `<li class='task flex-end' id='${toDo.index}'>
+    <div class='content completed flex-end' > 
+    <input type='checkbox' class='check-box' checked><p><span class='hide'>${toDo.task}</span></div>
+    <div class='icons '>
+    <button class='editBtn iconBtn '>
+    <i class="editBtn fas fa-pen btn  "></i>
+    </button>
+    <button class='trashBtn iconBtn ' >
+    <i class="trashBtn fas fa-trash btn"></i> 
+    </button>
+    </div></li>`;
+    } else {
+      toDoListItems += `<li class='task flex-end' id='${toDo.index}'>
+    <div class='content flex-end ' > 
+    <input type='checkbox' class='check-box'><p> <span class='hide'>${toDo.task}</span></p></div>
+    <div class='icons'>
+    <button class='editBtn iconBtn ' >
+    <i class="editBtn fas fa-pen btn"></i>
+    </button>
+    <button class='iconBtn trashBtn' >
+    <i class="trashBtn fas fa-trash btn"></i> 
+    </button>
+    </div>
+    </li>`;
+    }
+  }
+  toDoList.innerHTML = toDoListItems;
+}
+
+document.addEventListener('DOMContentLoaded', loadTodo);
